b872c975bb624cd2e45645d34fb7c686e3febead
Rx5557770 <69506638+Rx5557770@users.noreply.github.com>
2023-12-26 18:52:30 +0800

    CheckIP.py

--- CheckIP.py
+++ CheckIP.py
@@ -2,6 +2,11 @@
 import requests
 import os
 
+# 脚本自動識別當前目錄下的 IPS.txt 文件，若不存在則手動輸入文件夾路徑，注意：路徑後面需要加 /
+# 獲取cloudflare的反代IP，保存到 Success_IPS.txt 中
+# 獲取失敗的IP，保存到 Fail_IPS.txt 中
+# 探測失敗的IP，保存到 Continue_IPS.txt 中
+
 # 保存文件路徑
 Success_PATH = os.getcwd() + '/Success_IPS.txt'
 Fail_PATH = os.getcwd() + '/Fail_IPS.txt'
@@ -78,8 +83,3 @@ else:
                 print('Fail:' + IP)
                 with open(Fail_PATH, 'a+') as t:
                     t.write(IP + '\n')
-
-
-
-
-
